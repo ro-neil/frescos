f1bf849d3721b1d329fbfb28e6a9bff819efc691
ro-neil <roneilbarrett@gmail.com>
2026-06-16 17:52:22 -0400

    fixed component path error

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Leadtext from '@/components/home/Leadtext.vue';
+import Leadtext from '@/components/shared/Leadtext.vue';
 import NavigationBar from '@/components/shared/NavigationBar.vue'
 </script>
 
