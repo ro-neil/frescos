7234cc9cc400d3ee9054d0e248185b8e7e689772
ro-neil <roneilbarrett@gmail.com>
2026-08-20 13:00:05 -0400

    Refactored VenueBlock to remove unused code. Added a specific 'venue-display' image

--- src/components/booking/VenueBlock.vue
+++ src/components/booking/VenueBlock.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import ContentBlock from '@/components/shared/ContentBlock.vue';
-import Link from '@/components/utils/Link.vue';
-import servingWine from '@/assets/images/catering/serving-wine.jpg';
-import diningRoom1 from '@/assets/images/catering/dining-room-1.jpg';
-import diningRoom2 from '@/assets/images/catering/dining-room-2.jpg';
-import tableAesthetics from '@/assets/images/catering/table-aesthetics.jpg';
-import outdoorDining from '@/assets/images/catering/outdoor-dining.jpg';
-import Subtext from '@/components/shared/Subtext.vue';
-import Leadtext from '@/components/shared/Leadtext.vue';
+import venueDisplay from '@/assets/images/catering/venue-display.jpg';
 
 // Define props for full extensibility
 interface Image {
@@ -16,28 +7,15 @@ interface Image {
   alt: string;
 }
 
-const imageA: Image = {
-  path: diningRoom1,
+const image: Image = {
+  path: venueDisplay,
   alt: "A photo of an indoor dining area.",
-}
-
-const imageB: Image = {
-  path: outdoorDining,
-  alt: "A photo of an outdoor dining area.",
-}
-
-const imageC: Image = {
-  path: tableAesthetics,
-  alt: "A photo of an aesthetically appealing table.",
 }
 
 </script>
 
 <template>
   <div class="w-full shadow-sm shadow-gray-200 transform transition duration-500 hover:scale-[1.02]">
-    <!-- <div class="my-4! flex justify-center">
-      <div class="w-32 md:w-64 h-1.5 bg-orange-400 rounded-full"></div>
-    </div> -->
-    <img :src="imageA.path" :alt="imageA.alt" loading="eager" class="object-cover saturate-200 w-full max-h-150">
+    <img :src="image.path" :alt="image.alt" loading="eager" class="object-cover saturate-200 w-full max-h-150">
   </div>
 </template>
